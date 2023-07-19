ea262c04f46dfae1da5e89acde35769e45b52efe
namilhand <namilhand@gmail.com>
2023-07-19 22:01:00 +0900

    bedg of nonCG

--- src/cx2bedGraph.sh
+++ src/cx2bedGraph.sh
@@ -14,14 +14,16 @@ temp_dir=$4
 prefix=$(basename ${cx_input})
 prefix=${prefix%\.CX_report\.txt}
 
-available_context=("C" "CG" "CHG" "CHH")
+CNN_context=("C" "CG" "CHG" "CHH")
 
 mkdir -p $dir_output
 
 if [[ $context = "C" ]]; then
     awk 'BEGIN{FS="\t"; OFS="\t";} (( $4 + $5 ) >= 4) {chrom=$1; start=$2-1; end=$2; score=( $4 / ( $4 + $5 )); print chrom, start, end, score}' $cx_input | sort -T $temp_dir -k1,1 -k2,2n | sed '1s/^/track type=bedGraph\n/' > ${dir_output}/${prefix}.${context}.bedg
-elif [[ ${available_context[@]} =~ $context ]]; then
+elif [[ ${CNN_context[@]} =~ $context ]]; then
     awk -v context=$context 'BEGIN{FS="\t"; OFS="\t";} (( $6 == context )) && (( $4 + $5 ) >= 4) {chrom=$1; start=$2-1; end=$2; score=( $4 / ( $4 + $5 )); print chrom, start, end, score}' $cx_input | sort -T $temp_dir -k1,1 -k2,2n | sed '1s/^/track type=bedGraph\n/' > ${dir_output}/${prefix}.${context}.bedg 
+elif [[ $context == "nonCG" ]]; then
+    awk 'BEGIN{FS="\t"; OFS="\t";} (( $6 == "CHG" ) || ( $6 == "CHH" )) && (( $4 + $5 ) >= 4) {chrom=$1; start=$2-1; end=$2; score=( $4 / ( $4 + $5 )); print chrom, start, end, score}' $cx_input | sort -T $temp_dir -k1,1 -k2,2n | sed '1s/^/track type=bedGraph\n/' > ${dir_output}/${prefix}.${context}.bedg 
 else
-    echo context should be one of C, CG, CHG, or CHH
+    echo context should be one of C, CG, CHG, CHH, or nonCG
 fi
